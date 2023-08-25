9fd92b7c8cf9be833bbb8774f94b41b5339eb08d
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-25 16:41:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers"; // Import your root reducer
 import App from "./components/App";
 
-const store = createStore(rootReducer); // Create the Redux store with your root reducer
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
